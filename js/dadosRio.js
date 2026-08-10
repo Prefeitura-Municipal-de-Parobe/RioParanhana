e2e6b3e3f398833c5f7e1c162dafc44febcb45dc
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2026-08-10 14:04:14 -0300

    Rive data script update

--- js/dadosRio.js
+++ js/dadosRio.js
@@ -35,6 +35,37 @@ const mapWeatherData = (riverLevel, weatherSummaryData) => {
     nivelRio = riverLevel;
 };
 
+const checkWindDirection = (originDirection) => {
+    
+    switch(originDirection){
+        case "N": return 0;
+        case "NNE": return 22.5;
+        case "NE": return 45;
+        case "ENE": return 67.5;
+        case "E": return 90;
+        case "ESE": return 112.5;
+        case "SE": return 135;
+        case "SSE": return 157.5;
+        case "S": return 180;
+        case "SSW": 
+        case "SSO": return 202.5;
+        case "SW": 
+        case "SO": return 225;
+        case "WSW": 
+        case "OSO": return 247.5;
+        case "W": 
+        case "O": return 270;
+        case "WNW": 
+        case "ONO": return 292.5;
+        case "NW": 
+        case "NO": return 315;
+        case "NNW": 
+        case "NNO": return 337.5;
+        default: return null;
+    }
+
+}
+
 async function fetchRiverData() {
   try {
     const riverLevelRes = await fetch('/api/rio-proxy?path=river/level');
@@ -61,7 +92,7 @@ function renderizarDadosFluviais(riverLevel, dados) {
     document.querySelector('#TempAtual').innerText = `${dados.tempAtual}°c`;
     document.querySelector('#SensTerm').innerText = `${dados.sensTerm}°c`;
     document.querySelector('#VeloVento').innerText = `${dados.veloVento}Km/H`;
-    document.querySelector('#compass-needle').style.rotate = `${checkWindDirection(dados.direVento)}deg`;
+    document.querySelector('#compass-needle').style.rotate = `${checkWindDirection(dados.direVento) - 180}deg`;
 }
 
 function renderizarGraficos(riverLevel, dados) {
